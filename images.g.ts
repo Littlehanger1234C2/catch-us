26808b9b3ab13b16c04e07c635286fcd1720bb1f
Littlehanger1234 <74791991+Littlehanger1234C2@users.noreply.github.com>
2021-04-04 11:36:03 -0400

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,84 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":return img`
+. . 2 2 2 2 . . 
+. 2 2 2 2 2 2 . 
+2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 
+. 2 2 2 2 2 2 . 
+. . 2 2 2 2 . . 
+`;
+            case "image2":return img`
+. . 2 2 2 2 . . 
+. 2 2 2 2 2 2 . 
+2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 
+. 2 2 2 2 2 2 . 
+. . 2 2 2 2 . . 
+`;
+            case "image3":return img`
+. . 8 8 8 8 . . 
+. 8 8 8 8 8 8 . 
+8 8 8 8 8 8 8 8 
+8 8 8 8 8 8 8 8 
+8 8 8 8 8 8 8 8 
+8 8 8 8 8 8 8 8 
+. 8 8 8 8 8 8 . 
+. . 8 8 8 8 . . 
+`;
+            case "image5":return img`
+. . . . . . . . . . . . . . . . 
+. 8 8 8 8 8 8 8 1 1 1 1 1 . . . 
+8 8 8 8 8 8 8 8 1 1 1 1 1 1 . . 
+8 8 8 8 8 8 8 8 1 1 1 1 1 1 . . 
+8 8 8 8 8 8 8 8 1 1 1 1 1 1 . . 
+8 8 8 8 8 1 8 8 1 1 1 1 1 1 . . 
+8 8 8 8 8 1 1 8 1 1 1 1 1 1 . . 
+8 8 8 8 8 1 1 1 1 1 1 1 1 1 . . 
+8 8 8 8 8 1 1 1 1 1 1 1 1 1 . . 
+8 8 8 8 8 1 1 8 1 1 1 1 1 1 . . 
+8 8 8 8 8 1 8 8 1 1 1 1 1 1 . . 
+8 8 8 8 8 8 8 8 1 1 1 1 1 1 . . 
+8 8 8 8 8 8 8 8 1 1 1 1 1 1 . . 
+. 8 8 8 8 8 8 8 1 1 1 1 1 . . . 
+. . . 8 8 8 . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image4":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+f f f f f f f f f f f f f f f f 
+f 7 5 4 2 a 8 7 5 4 2 a 8 7 5 f 
+f 7 5 4 2 a 8 7 5 4 2 a 8 7 5 f 
+f 7 5 4 2 a 8 7 5 4 2 a 8 7 5 f 
+f 7 5 4 2 a 8 7 5 4 2 a 8 7 5 f 
+f 7 5 4 2 a 8 7 5 4 2 a 8 7 5 f 
+f 7 5 4 2 a 8 7 5 4 2 a 8 7 5 f 
+f 7 5 4 2 a 8 7 5 4 2 a 8 7 5 f 
+f 7 5 4 2 a 8 7 5 4 2 a 8 7 5 f 
+f f f f f f f f f f f f f f f f 
+. . . . . . . . . . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
